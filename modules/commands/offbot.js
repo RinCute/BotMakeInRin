70ef9afc84390eabe6930f1e1f59bc58fe61b9b2
RinCute <92521355+RinCute@users.noreply.github.com>
2021-10-16 18:49:12 +0700

    Update offbot.js

--- modules/commands/offbot.js
+++ modules/commands/offbot.js
@@ -7,4 +7,4 @@ module.exports.config = {
 	commandCategory: "system",
 	cooldowns: 0
         };
-module.exports.run = ({event, api}) =>api.sendMessage("Bye Mọi Người, Bot Của Bảo Luân Đi Ngủ Đây <3",event.threadID, () =>process.exit(0))
+module.exports.run = ({event, api}) =>api.sendMessage("Bye Mọi Người, Bot Của Hiển Rin Đi Ngủ Đây <3",event.threadID, () =>process.exit(0))
